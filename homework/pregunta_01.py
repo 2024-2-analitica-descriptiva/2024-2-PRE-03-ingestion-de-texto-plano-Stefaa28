58a0ee42141aac53f321aa8210ece8f6107bf5ca
Stefaa28 <esacevedoco@unal.edu.co>
2025-01-10 19:55:05 -0500

    Lab3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
 
 def pregunta_01():
     """
@@ -18,3 +19,40 @@ def pregunta_01():
 
 
     """
+    #Lectura del archivo 
+    with open('files/input/clusters_report.txt','r', encoding = 'utf-8') as file:
+        lineas = file.readlines()
+
+     # Procesar las líneas del archivo omitiendo las primeras cuatro líneas de encabezado
+    texto = lineas[4:]
+    
+    #Definir las columnas en minúsculas, sin espacios y reemplazando por _  
+    columnas = [["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave","principales_palabras_clave"]]
+    
+    # Inicializar listas para almacenar los datos
+    lista = []
+
+    p = True
+      
+    for linea in texto:
+        linea.strip()
+        linea = linea.split()
+        print(linea)
+        if len(linea) > 0 and p:
+            lista.append(int(linea[0]))
+            lista.append(int(linea[1]))
+            lista.append(float(linea[2].replace(',','.')))
+            lista.append(" ".join(linea[4:]))
+            p = False
+        
+        elif len(linea) > 0:
+            lista.append(" ".join(linea))
+            
+        else:
+            p =True
+            lista[3] = ' '.join(lista[3:]).replace('.', '')
+            columnas.append(lista[:4])
+            lista = []    
+    return pd.DataFrame(columnas[1:], columns = columnas[0]) 
+
+print(pregunta_01())
